7231e1b78579dddcd746b5ef3cc8168b7b7c0744
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-11-29 16:42:28 +0100

    Add initial 1.1.3 - function split

---
+++ csharp/jetbrains_rider/algo_05/ex_1_1_3_number_average/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace ex_1_1_3_number_average
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string listNumber;
+            int[] listNum;
+
+            listNumber = "10 12 115 9 33 6748 45";
+
+            listNum = Program.Split(listNumber, ' ');
+        }
+
+        private static int[] Split(string _sentence, char _charWhichSplit)
+        {
+            int howManyNumbers;
+            int numbersCounter;
+            int[] listNumbers;
+            string numberBeforeCast;
+
+            howManyNumbers = 0;
+            for (int charCounter = 0; charCounter < _sentence.Length; charCounter++)
+            {
+                if (_sentence[charCounter] == _charWhichSplit)
+                {
+                    howManyNumbers++;
+                }
+            }
+
+            howManyNumbers++;
+
+            numbersCounter = 0;
+            numberBeforeCast = "";
+            listNumbers = new int[howManyNumbers];
+
+            for (int charCounter = 0; charCounter < _sentence.Length; charCounter++)
+            {
+                if (_sentence[charCounter] != _charWhichSplit)
+                {
+                    numberBeforeCast = numberBeforeCast + _sentence[charCounter];
+                }
+
+                if (_sentence[charCounter] == _charWhichSplit | charCounter == (_sentence.Length - 1))
+                {
+                    listNumbers[numbersCounter] = int.Parse(numberBeforeCast);
+                    numbersCounter++;
+                    numberBeforeCast = "";
+                }
+            }
+
+            return listNumbers;
+        }
+    }
+}
