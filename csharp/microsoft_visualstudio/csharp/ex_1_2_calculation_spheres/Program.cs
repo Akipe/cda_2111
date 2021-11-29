eb577b54201efa2f54a13412a474c0427b942e98
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-11-29 16:02:59 +0100

    Add csharp exercice 1.2

---
+++ csharp/microsoft_visualstudio/csharp/ex_1_2_calculation_spheres/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex_1_2_calculation_spheres
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
