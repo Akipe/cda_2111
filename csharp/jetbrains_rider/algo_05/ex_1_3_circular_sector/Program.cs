843d0d9f04005123664024e2ed01706eaa2e9f1d
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-11-30 08:50:24 +0100

    Add csharp exercice 1.3 circular sector

---
+++ csharp/jetbrains_rider/algo_05/ex_1_3_circular_sector/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ex_1_3_circular_sector
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            double radius;
+            double angle;
+            double surface;
+            
+            Console.WriteLine("Welcome to the circular sector calculator.");
+            radius = Helper.GetDoubleFromUser("Please enter the radius of the circular sector :");
+            angle = Helper.GetDoubleFromUser("Please enter the angle of the circular sector :");
+
+            surface = GetSurfaceFromCircularSector(radius, angle);
+            
+            Console.WriteLine($"The surface of the circular sector is {surface} .");
+        }
+
+        private static double GetSurfaceFromCircularSector(double radius, double angle)
+        {
+            return (Math.PI * Math.Pow(radius, 2) * angle) / 360;
+        }
+    }
+}
