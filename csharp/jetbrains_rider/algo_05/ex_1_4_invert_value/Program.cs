6012d71450cecc6f87a5e2e379a205c7c25f9e4b
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-11-30 09:26:07 +0100

    Add exercice csharp 1.4 invert 2 vars

---
+++ csharp/jetbrains_rider/algo_05/ex_1_4_invert_value/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using ex_1_3_circular_sector;
+
+namespace ex_1_4_invert_value
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber;
+            int secondNumber;
+            int tempStorage;
+            
+            Console.WriteLine("Welcome !");
+            
+            firstNumber = Helper.GetIntFromUser("Please enter the first number :");
+            secondNumber = Helper.GetIntFromUser("Please enter the second number :");
+
+            tempStorage = firstNumber;
+            firstNumber = secondNumber;
+            secondNumber = tempStorage;
+            
+            Console.WriteLine($"Now the first number is {firstNumber} and the second number is {secondNumber}");
+            Console.WriteLine("Bye bye !");
+        }
+    }
+}
