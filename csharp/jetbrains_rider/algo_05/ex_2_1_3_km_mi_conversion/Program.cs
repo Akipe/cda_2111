26f806c382ad19144b319257d84414d8db7cf280
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-01 11:40:17 +0100

    Add initial exercice 2.1.3 conversion km mi

---
+++ csharp/jetbrains_rider/algo_05/ex_2_1_3_km_mi_conversion/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace ex_2_1_3_km_mi_conversion
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            string userCommand;
+            
+            Console.WriteLine("Programs for convert kilometers <-> Miles.");
+            
+            Console.WriteLine("Please enter your distance to convert with unit (\"km\" or \"mi\", if no units define it will be km)");
+            Console.WriteLine("Or \"go\" to execute conversion");
+            Console.WriteLine("Or \"qui\" to exit.");
+
+            userCommand = Console.ReadLine();
+            int.TryParse(userCommand, out number);
+
+            if (HasUserEnterDistance(userCommand, out distance, out unit))
+            {
+            }
+        }
+
+        private static bool HasUserEnterDistance(string userCommand, out double distance, out string unit)
+        {
+            bool asSpaceInUserCommand;
+            string maybeDistance;
+            string maybeUnit;
+            double distance;
+
+            asSpaceInUserCommand = false;
+            try
+            {
+                for (int index = 0; index < userCommand.Length; index++)
+                {
+                    if (userCommand[index] == ' ')
+                    {
+                        maybeDistance = userCommand.Substring(0, index - 1);
+                        distance = GetDistanceIfIsCorrect(maybeDistance);
+                        
+                        // Try to guest unit right of index
+                        maybeUnit = userCommand.Substring(index + 1, userCommand.Length);
+
+                        asSpaceInUserCommand = true;
+                    }
+                }
+
+                if (!asSpaceInUserCommand)
+                {
+                    // Try to cast all usercommand
+                }
+            }
+        }
+        private static double GetDistanceIfIsCorrect(string maybeDistance)
+        {
+            double distance;
+            
+            distance = double.Parse(maybeDistance);
+
+            if (DistanceExceedLimit(distance))
+            {
+                throw new Exception(
+                    "Error : the distance exceed the limit (smaller than 0.01 or bigger than 1 000 000.");
+            }
+
+            return distance;
+        }
+
+        private static bool DistanceExceedLimit(double distance)
+        {
+            return distance < 0.01 | distance > 1000000;
+        }
+    }
+    
+
+}
