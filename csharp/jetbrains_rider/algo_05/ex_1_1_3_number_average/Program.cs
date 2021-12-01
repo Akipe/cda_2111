83b023cc4b328e7589d3a933fa812d02835a1aae
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-01 11:44:18 +0100

    Add doc for split methode & seperate code for split

--- csharp/jetbrains_rider/algo_05/ex_1_1_3_number_average/Program.cs
+++ csharp/jetbrains_rider/algo_05/ex_1_1_3_number_average/Program.cs
@@ -9,7 +9,6 @@ namespace ex_1_1_3_number_average
             string userInputNumbers;
             int howManyNumbers;
             int sumNumbers;
-            int countNumbers;
             float averageNumber;
             int[] listNumbers;
 
@@ -29,17 +28,29 @@ namespace ex_1_1_3_number_average
             Console.WriteLine($"The average is {averageNumber}");
         }
 
-        private static int[] Split(string _sentence, char _charWhichSplit)
+        /// <summary>
+        /// Split a string to a static array with numbers separate by a char.
+        /// First version without optimizations (addition of strings).
+        /// </summary>
+        /// <param name="sentence">The string to split</param>
+        /// <param name="charWhichSplit">The char who separates numbers</param>
+        /// <returns>Return a static array with numbers inside the string.</returns>
+        private static int[] Split(string sentence, char charWhichSplit)
         {
             int howManyNumbers;
-            int numbersCounter;
-            int[] listNumbers;
-            string numberBeforeCast;
+            
+            howManyNumbers = Program.SplitGetHowManyNumbers(sentence, charWhichSplit);
+            return Program.SplitGetNumbersListNotOptimizeString(sentence, howManyNumbers, charWhichSplit);
+        }
+
+        private static int SplitGetHowManyNumbers(string sentence, char charWhichSplit)
+        {
+            int howManyNumbers;
 
             howManyNumbers = 0;
-            for (int charCounter = 0; charCounter < _sentence.Length; charCounter++)
+            for (int charCounter = 0; charCounter < sentence.Length; charCounter++)
             {
-                if (_sentence[charCounter] == _charWhichSplit)
+                if (sentence[charCounter] == charWhichSplit)
                 {
                     howManyNumbers++;
                 }
@@ -47,26 +58,35 @@ namespace ex_1_1_3_number_average
 
             howManyNumbers++;
 
+            return howManyNumbers;
+        }
+
+        private static int[] SplitGetNumbersListNotOptimizeString(string sentence, int howManyNumbers, char charWhichSplit)
+        {
+            int[] numberslist;
+            int numbersCounter;
+            string numberBeforeCast;
+            
             numbersCounter = 0;
             numberBeforeCast = "";
-            listNumbers = new int[howManyNumbers];
+            numberslist = new int[howManyNumbers];
 
-            for (int charCounter = 0; charCounter < _sentence.Length; charCounter++)
+            for (int charCounter = 0; charCounter < sentence.Length; charCounter++)
             {
-                if (_sentence[charCounter] != _charWhichSplit)
+                if (sentence[charCounter] != charWhichSplit)
                 {
-                    numberBeforeCast = numberBeforeCast + _sentence[charCounter];
+                    numberBeforeCast = numberBeforeCast + sentence[charCounter];
                 }
 
-                if (_sentence[charCounter] == _charWhichSplit | charCounter == (_sentence.Length - 1))
+                if (sentence[charCounter] == charWhichSplit | charCounter == (sentence.Length - 1))
                 {
-                    listNumbers[numbersCounter] = int.Parse(numberBeforeCast);
+                    numberslist[numbersCounter] = int.Parse(numberBeforeCast);
                     numbersCounter++;
                     numberBeforeCast = "";
                 }
             }
 
-            return listNumbers;
+            return numberslist;
         }
     }
 }
