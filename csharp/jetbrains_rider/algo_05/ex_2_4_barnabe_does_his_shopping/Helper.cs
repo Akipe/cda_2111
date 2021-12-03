a86fa56a3dcb79c71144c54dabba6fd8b9009c89
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-03 11:38:28 +0100

    Add exercice csharp 2.4 Barnabé does his shopping

---
+++ csharp/jetbrains_rider/algo_05/ex_2_4_barnabe_does_his_shopping/Helper.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace ex_2_4_barnabe_does_his_shopping
+{
+    public static class Helper
+    {
+        public static float GetFloatFromUser(string message)
+        {
+            string userInput;
+            
+            do
+            {
+                Console.WriteLine(message);
+
+                try
+                {
+                    userInput = Console.ReadLine();
+                    if (userInput == null)
+                    {
+                        throw new ArgumentException();
+                    }
+                    return float.Parse(userInput);
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine($"Error: please enter a correct number ({error.Message})");
+                }
+                catch (ArgumentNullException error)
+                {
+                    Console.WriteLine($"Error: please enter a number ({error.Message})");
+                }
+            } while (true);
+        }
+    }
+}
---
+++ csharp/jetbrains_rider/algo_05/ex_2_4_barnabe_does_his_shopping/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace ex_2_4_barnabe_does_his_shopping
+{
+    internal static class Program
+    {
+        public static void Main(string[] args)
+        {
+            float currentBarnabeMoney;
+            int howManyStoreBarnabeDoesShopping;
+            
+            Console.WriteLine("Welcome to Barnabé Shopping Simulator!");
+            currentBarnabeMoney = Helper.GetFloatFromUser("Enter the current money of Barnabé :");
+            howManyStoreBarnabeDoesShopping = BarnabeeAlgorithm(currentBarnabeMoney);
+
+            Console.WriteLine($"Barnabe does shopping to {howManyStoreBarnabeDoesShopping} stores !");
+            Console.WriteLine("Bye bye!");
+        }
+
+        /// <summary>
+        /// Get the numbers of store where Barnabé does his shopping.
+        /// Following this statement (in french) :
+        /// "Dans chacun, il a dépensé 1 euro de plus que la moitié de ce qu’il avait en entrant. Dans le dernier magasin, il dépense le solde."
+        /// </summary>
+        /// <param name="money">The initial money of Barnabé, has to be bigger than 1</param>
+        /// <returns>How many stores Barnabé does his shopping</returns>
+        private static int BarnabeeAlgorithm(float money)
+        {
+            int howManyStores;
+
+            if (money <= 1)
+            {
+                throw new ArgumentException("The initial money of Barnabé has to be bigger than 1 euro");
+            }
+            
+            howManyStores = 0;
+
+            while (true)
+            {
+                if (money > (money / 2 + 1))
+                {
+                    money = money - (money / 2 + 1);
+                    howManyStores++;
+                }
+                else
+                {
+                    // The last store, because his no more money
+                    howManyStores++;
+                    return howManyStores;
+                }    
+            }
+        }
+    }
+}
