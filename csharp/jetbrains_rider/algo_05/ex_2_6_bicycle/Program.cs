a892d2d4b86e7345cd65c9841890230fb4112d89
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-06 15:36:28 +0100

    Add initial exercice csharp 2.5 bicycle, initial variables

---
+++ csharp/jetbrains_rider/algo_05/ex_2_6_bicycle/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex_2_6_bicycle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool weatherIsGood;
+            bool bicyleIsGoodShape;
+            bool repairsAreFast;
+            bool doIOwnGameOfThrones;
+            bool libraryOwnsGameOfThrones;
+        }
+    }
+}
