ac1a8249461a94f62b282e1c8e19aa23f8f84f76
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-06 15:47:03 +0100

    Ex chsarp 2.6 add base algorithm and fix typo for a variable

--- csharp/jetbrains_rider/algo_05/ex_2_6_bicycle/Program.cs
+++ csharp/jetbrains_rider/algo_05/ex_2_6_bicycle/Program.cs
@@ -7,10 +7,29 @@ namespace ex_2_6_bicycle
         static void Main(string[] args)
         {
             bool weatherIsGood;
-            bool bicyleIsGoodShape;
+            bool bicycleIsGoodShape;
             bool repairsAreFast;
             bool doIOwnGameOfThrones;
             bool libraryOwnsGameOfThrones;
+            
+            // If the weather is good
+                // If the bicycle is in good shape
+                    // I will ride my bike
+                // But if bicycle is not in good shape
+                    // If repairs are fast
+                        // I will ride my bike
+                    // Else
+                        // I will walk
+            // Else if the weather is not good, i will read the book Game of Thrones
+                // If I have the book
+                    // I take the book GOT
+                // Else I haven't the book
+                    // If the library owns the book
+                        // I borrow GOT
+                    // Else the library doesn't owns the book
+                        // I borrow a detective story
+                    // I go back home
+                // I will sit and read the book
         }
     }
 }
