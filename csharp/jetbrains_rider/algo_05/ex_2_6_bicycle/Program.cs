5f9e2386c25d809ed538eeb5ddbb0ed75d8c5b17
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-06 16:49:03 +0100

    Add final version of ex csharp 2.6 bicycle

--- csharp/jetbrains_rider/algo_05/ex_2_6_bicycle/Program.cs
+++ csharp/jetbrains_rider/algo_05/ex_2_6_bicycle/Program.cs
@@ -2,34 +2,106 @@
 
 namespace ex_2_6_bicycle
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            bool weatherIsGood;
-            bool bicycleIsGoodShape;
-            bool repairsAreFast;
-            bool doIOwnGameOfThrones;
-            bool libraryOwnsGameOfThrones;
-            
             // If the weather is good
+            if (AskUser("Is the weather is good ?"))
+            {
+                Console.WriteLine("I try my bike...");
+                
                 // If the bicycle is in good shape
+                if (AskUser("If the bibcyle is in good shape ?"))
+                {
                     // I will ride my bike
+                    Console.WriteLine("I will ride my bike.");
+                }
                 // But if bicycle is not in good shape
+                else
+                {
+                    Console.WriteLine("The bicycle needs to be repair.");
+                    
                     // If repairs are fast
+                    if (AskUser("Is repairs are fast ?"))
+                    {
                         // I will ride my bike
-                    // Else
+                        Console.WriteLine("I will ride the bike after the repairs.");
+                    }
+                    else
+                    {
                         // I will walk
+                        Console.WriteLine("I will walk.");
+                    }
+                }
+            }
             // Else if the weather is not good, i will read the book Game of Thrones
+            else
+            {
+                Console.WriteLine("I will read a book.");
+                
                 // If I have the book
+                if (AskUser("Do I have the book Game of Thrones ?"))
+                {
                     // I take the book GOT
+                    Console.WriteLine("I take the book.");
+                }
                 // Else I haven't the book
+                else
+                {
                     // If the library owns the book
+                    if (AskUser("Does the library has the book Game of Thrones ?"))
+                    {
                         // I borrow GOT
+                        Console.WriteLine("I borrow the book Game of Thrones.");
+                    }
                     // Else the library doesn't owns the book
-                        // I borrow a detective story
+                    else
+                    {
+                        // I borrow a detective story book
+                        Console.WriteLine("I borrow a detective story book.");
+                    }
                     // I go back home
+                    Console.WriteLine("I go back home with the book...");
+                }
+                
                 // I will sit and read the book
+                Console.WriteLine("I will sit and read the book...");
+            }
+            
+            Console.WriteLine("I spent a beautiful day !");
+            Console.WriteLine("Bye bye!");
+        }
+
+        private static bool AskUser(string askMessage)
+        {
+            string userResponse;
+
+            do
+            {
+                try
+                {
+                    Console.WriteLine(askMessage + " (\"yes\" or \"no\")");
+                    userResponse = Console.ReadLine();
+
+                    if (String.IsNullOrWhiteSpace(userResponse))
+                    {
+                        throw new ApplicationException("You must enter a response.");
+                    }
+                
+                    switch (userResponse.Trim().ToLower())
+                    {
+                        case "yes": return true;
+                        case "no": return false;
+                        default: throw new ApplicationException("Your response is invalid.");
+                    }
+                }
+                catch (ApplicationException error)
+                {
+                    Console.WriteLine($"Error : {error.Message}\n Please enter \"yes\" or \"no\"");
+                }
+                
+            } while (true);
         }
     }
 }
