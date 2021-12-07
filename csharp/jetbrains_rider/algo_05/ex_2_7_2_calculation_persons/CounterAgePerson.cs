da9be0694076912fd26e28ee01a15f3e3773f398
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-07 09:24:58 +0100

    Add exercice csharp 2.7.2 calculation of persons

---
+++ csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/CounterAgePerson.cs
@@ -0,0 +1,20 @@
+﻿namespace ex_2_7_2_calculation_persons
+{
+    public class CounterAgePerson
+    {
+        /*
+         * Algorithm for count young people.
+         * 1) Save user entry for all people.
+         * 2) For each people, ask age, and save it to an array.
+         * 3) After,make a loop for all ages.
+         * 4) If a person has less than 20 years old, increase to one a counter of young people.
+         * 5) If a person has 20 years old, increase to one a counter of people who are 20 years old.
+         * 6) If a person has more than 20 years old, increase to one a counter of adult persons.
+         * 7) If the counter of young people is same as the number of people, show "All people are less than 20 years old"
+         * 8) If the counter of adult persons is same as the number of people, show "All people are more than 20 years old"
+         * 9) After the loop, show how many people are young with "- than 20",
+         *      how many people are 20 years old with "= than 20",
+         *      and how many people are adults with "+ than 20".
+         */
+    }
+}
---
+++ csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ex_2_7_2_calculation_persons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
