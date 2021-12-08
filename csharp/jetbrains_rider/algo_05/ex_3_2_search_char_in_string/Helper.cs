052cc2c7799a46780c059a42c5b966d2c2cd9df0
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-08 16:49:42 +0100

    Add exercice csharp 3.2 search character in an array

---
+++ csharp/jetbrains_rider/algo_05/ex_3_2_search_char_in_string/Helper.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace ex_3_2_search_char_in_string
+{
+    public static class Helper
+    {
+        public static char GetCharFromUser(string _message)
+        {
+            string userInput;
+            
+            do
+            {
+                Console.WriteLine(_message);
+
+                try
+                {
+                    userInput = Console.ReadLine();
+                    if (userInput == null)
+                    {
+                        throw new ArgumentException("Please enter a character !");
+                    }
+
+                    if (userInput.Length > 1)
+                    {
+                        throw new ArgumentException("Please enter only one character !");
+                    }
+                    
+                    return userInput[0];
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine($"Error: please enter a correct character ({error.Message})");
+                }
+                catch (ArgumentNullException error)
+                {
+                    Console.WriteLine($"Error: please enter a character ({error.Message})");
+                }
+            } while (true);
+        }
+    }
+}
