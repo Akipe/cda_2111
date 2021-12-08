740fdfd76cde7e5939216661f0725acc29bdd5e4
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-08 14:01:54 +0100

    Add final working exercice csharp 2.7.2

--- csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/CounterAgePerson.cs
+++ csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/CounterAgePerson.cs
@@ -1,4 +1,6 @@
-﻿namespace ex_2_7_2_calculation_persons
+﻿using System;
+
+namespace ex_2_7_2_calculation_persons
 {
     public class CounterAgePerson
     {
@@ -16,5 +18,164 @@
          *      how many people are 20 years old with "= than 20",
          *      and how many people are adults with "+ than 20".
          */
+        
+        private const int MINIMAL_AGE_ALLOW = 1;
+        private const int MAXIMAL_AGE_ALLOW = 120;
+        private const int LIMIT_AGE_YOUNG_ADULT = 20;
+        
+        private int[] _agePeople;
+        private bool _countHasBeenDone;
+        private int _youngPeople;
+        private int _twentyYearsOldPeople;
+        private int _adultPeople;
+        
+        public CounterAgePerson()
+        {
+            this._agePeople = new int[] {0};
+            this._countHasBeenDone = false;
+            this.InitializeCounters();
+        }
+
+        public void AddPerson(int age)
+        {
+            this.ThrowErrorIfLowerThanMinimumAgeAccepted(age);
+            this.ThrowErrorIfBiggerThanMaximalAgeAccepted(age);
+            
+            if (!this.IsThereAlreadyRegisteredPerson())
+            {
+                this._agePeople[0] = age;
+            }
+            else
+            {
+                int[] agePeopleIncrease = new int[this.GetHowManyPeople() + 1];
+                
+                Array.Copy(this._agePeople, agePeopleIncrease, this._agePeople.Length);
+
+                this._agePeople = agePeopleIncrease;
+                this._agePeople[^1] = age;
+            }
+
+            this.ResetCounterHasBeenDone();
+        }
+
+        public int GetHowManyYoungPeople()
+        {
+            this.MakeAgeCountIfHasNotBeenDone();
+
+            return this._youngPeople;
+        }
+
+        public int GetHowManyTwentyYearsOldPeople()
+        {
+            this.MakeAgeCountIfHasNotBeenDone();
+
+            return this._twentyYearsOldPeople;
+        }
+
+        public int GetHowManyAdults()
+        {
+            this.MakeAgeCountIfHasNotBeenDone();
+
+            return this._adultPeople;
+        }
+
+        public bool AreAllTwentyYearsOldOrMore()
+        {
+            this.MakeAgeCountIfHasNotBeenDone();
+            
+            return this.GetHowManyYoungPeople() == 0 &
+                   (this.GetHowManyAdults() > 0 | this.GetHowManyTwentyYearsOldPeople() > 0);
+        }
+        
+        public bool AreAllYoung()
+        {
+            this.MakeAgeCountIfHasNotBeenDone();
+
+            return this.GetHowManyYoungPeople() == this.GetHowManyPeople();
+        }
+
+        private void MakeAgeCount()
+        {
+            this.InitializeCounters();
+            
+            foreach (int agePerson in _agePeople)
+            {
+                if (IsYoung(agePerson))
+                {
+                    this._youngPeople++;
+                }
+
+                if (IsAdult(agePerson))
+                {
+                    this._adultPeople++;
+                }
+
+                if (IsTwentyYearsOld(agePerson))
+                {
+                    this._twentyYearsOldPeople++;
+                }
+            }
+        }
+
+        private void ThrowErrorIfLowerThanMinimumAgeAccepted(int age)
+        {
+            if (age < CounterAgePerson.MINIMAL_AGE_ALLOW)
+            {
+                throw new ArgumentException(
+                    $"You must enter a age equal or bigger than {CounterAgePerson.MINIMAL_AGE_ALLOW}.");
+            }
+        }
+
+        private void ThrowErrorIfBiggerThanMaximalAgeAccepted(int age)
+        {
+            if (age > CounterAgePerson.MAXIMAL_AGE_ALLOW)
+            {
+                throw new ArgumentException(
+                    $"You must enter an age bellow or equal than {CounterAgePerson.MAXIMAL_AGE_ALLOW}.");
+            }
+        }
+
+        private bool IsThereAlreadyRegisteredPerson()
+        {
+            return this._agePeople[0] != 0;
+        }
+
+        private void MakeAgeCountIfHasNotBeenDone()
+        {
+            if (!this._countHasBeenDone)
+            {
+                this.MakeAgeCount();
+            }
+        }
+        private void ResetCounterHasBeenDone()
+        {
+            this._countHasBeenDone = false;
+        }
+
+        private bool IsAdult(int age)
+        {
+            return age > CounterAgePerson.LIMIT_AGE_YOUNG_ADULT;
+        }
+        
+        private bool IsYoung(int age)
+        {
+            return age < CounterAgePerson.LIMIT_AGE_YOUNG_ADULT;
+        }
+        private bool IsTwentyYearsOld(int age)
+        {
+            return age == CounterAgePerson.LIMIT_AGE_YOUNG_ADULT;
+        }
+
+        private void InitializeCounters()
+        {
+            this._youngPeople = 0;
+            this._twentyYearsOldPeople = 0;
+            this._adultPeople = 0;
+        }
+
+        private int GetHowManyPeople()
+        {
+            return this._agePeople.Length;
+        }
     }
 }
--- csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/Program.cs
+++ csharp/jetbrains_rider/algo_05/ex_2_7_2_calculation_persons/Program.cs
@@ -1,12 +1,83 @@
 ﻿using System;
+using System.Linq;
+using System.Text.RegularExpressions;
 
 namespace ex_2_7_2_calculation_persons
 {
-    class Program
+    internal static class Program
     {
-        static void Main(string[] args)
+        private const int HOW_MANY_AGES_NEEDED = 20;
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            CounterAgePerson personCounter = new CounterAgePerson();;
+            
+            Console.WriteLine("Welcome to the age people calculation.");
+
+            AddPeopleAgesFromUser(personCounter);
+            
+            if (personCounter.AreAllYoung())
+            {
+                Console.WriteLine("ALL PEOPLE ARE LESS THAN TWENTY YEARS OLD.");
+            }
+            else if (personCounter.AreAllTwentyYearsOldOrMore())
+            {
+                Console.WriteLine("ALL PEOPLE ARE MORE THAN TWENTY YEARS OLD.");
+            }
+            else
+            {
+                Console.WriteLine("Numbers of people with :");
+                Console.WriteLine($"- 20 : {personCounter.GetHowManyYoungPeople()}");
+                Console.WriteLine($"= 20 : {personCounter.GetHowManyTwentyYearsOldPeople()}");
+                Console.WriteLine($"+ 20 : {personCounter.GetHowManyAdults()}");
+            }
+        }
+
+        private static void AddPeopleAgesFromUser(CounterAgePerson counterPerson)
+        {
+            string userResponse;
+            string[] entriesUserResponse;
+            bool askAgain;
+            
+            do
+            {
+                askAgain = false;
+                
+                Console.WriteLine($"Please enter {HOW_MANY_AGES_NEEDED} ages of persons, separate with space :");
+                Console.WriteLine("(Example : \"20 14 12 26 23 24 10 14...\")");
+
+                try
+                {
+                    userResponse = Console.ReadLine();
+
+                    if (String.IsNullOrWhiteSpace(userResponse))
+                    {
+                        askAgain = true;
+                        throw new Exception($"Please enter a list of {HOW_MANY_AGES_NEEDED} ages.");
+                    }
+                    
+                    entriesUserResponse = Program.GetEntriesFromUserDistanceAndOptionalUnit(userResponse);
+
+                    if (entriesUserResponse.Length != HOW_MANY_AGES_NEEDED)
+                    {
+                        askAgain = true;
+                        throw new Exception($"Please enter a list of {HOW_MANY_AGES_NEEDED} ages.");
+                    }
+
+                    for (int nbEntryAgePerson = 0; nbEntryAgePerson < HOW_MANY_AGES_NEEDED; nbEntryAgePerson++)
+                    {
+                        counterPerson.AddPerson(int.Parse(entriesUserResponse[nbEntryAgePerson]));
+                    }
+                }
+                catch (Exception error)
+                {
+                    Console.WriteLine($"Error : {error.Message}");
+                }
+            } while (askAgain);
+        }
+
+        private static string[] GetEntriesFromUserDistanceAndOptionalUnit(string userResponse)
+        {
+            return userResponse.Trim().Split(" ");
         }
     }
 }
