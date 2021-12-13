f2ac710d5c70e0479de1aca411f05917539ab9b5
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-13 13:46:00 +0100

    Add exercice recursive csharp 1.3 2.10 palindrome

---
+++ csharp/algo_recursive_05b/ex_1_3_sub_2_10_palindromes/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex_1_3_sub_2_10_palindromes
+{
+    internal static class Program
+    {
+        public static void Main(string[] _args)
+        {
+            string wordToTest = "elle";
+            
+            Console.WriteLine($"Is the word \"{wordToTest}\" is a palindrome ? {IsPalindrome(wordToTest)}");
+        }
+
+        public static bool IsPalindrome(string _word)
+        {
+            if (_word.Length > 1)
+            {
+                if (_word[0] == _word[^1])
+                {
+                    // "_word.Length - 2" because we remove the first letter and the last letter.
+                    return IsPalindrome(_word.Substring(1, _word.Length - 2));
+                }
+                return false;
+            }
+
+            return true;
+        }
+    }
+}
