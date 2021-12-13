328a777c842f639b692afbf5e0bf3d7cfdb10990
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-13 13:18:55 +0100

    add exercice csharp recursive 1.2 2.7 calculation of power

---
+++ csharp/algo_recursive_05b/ex_1_2_sub_2_7_power_calculation/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ex_1_2_sub_2_7_power_calculation
+{
+    internal static class Program
+    {
+        public static void Main(string[] _args)
+        {
+            ulong a = 3;
+            ulong b = 20;
+            
+            Console.WriteLine($"The calculation of {a} power of {b} is equals to {PowerCalculation(a, b)}");
+        }
+
+        public static ulong PowerCalculation(ulong _number, ulong _power)
+        {
+            if (OperationAllowedIsEqualToZero(_power))
+            {
+                return 1;
+            }
+
+            return OperationAllowedMultiplication(_number, PowerCalculation(_number, OperationAllowedSubtractOne(_power)));
+        }
+
+        private static ulong OperationAllowedMultiplication(ulong _numberA, ulong _numberB)
+        {
+            return _numberA * _numberB;
+        }
+
+        private static ulong OperationAllowedSubtractOne(ulong _numberA)
+        {
+            return _numberA -  1;
+        }
+
+        private static bool OperationAllowedIsEqualToZero(ulong _numberA)
+        {
+            return _numberA == 0;
+        }
+    }
+}
