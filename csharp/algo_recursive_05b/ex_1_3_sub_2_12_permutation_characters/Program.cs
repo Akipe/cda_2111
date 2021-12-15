3ed011ad1764f792a27168ad7bc03f2f3eb5a6ed
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-15 10:26:43 +0100

    Add final version of ex 1.3 2.12 permutation

--- csharp/algo_recursive_05b/ex_1_3_sub_2_12_permutation_characters/Program.cs
+++ csharp/algo_recursive_05b/ex_1_3_sub_2_12_permutation_characters/Program.cs
@@ -6,83 +6,36 @@ namespace ex_1_3_sub_2_12_permutation_characters
     {
         public static void Main(string[] _argv)
         {
-            string wordToPermut = "abce";
-
-            string beforeLetter;
-            string afterLetter;
-            string letter;
-            string wordWithoutLetter;
+            string wordToPermut = "abcde";
+            int counter = 100;
             
-            // beforeLetter = wordToPermut.Substring(0, 0);
-            // letter = wordToPermut.Substring(0, 1);
-            // afterLetter = wordToPermut.Substring(1, wordToPermut.Length - 1);
-            // wordWithoutLetter = beforeLetter + afterLetter;
-            // Console.WriteLine("Select "+ letter);
-            // Console.WriteLine("(before)0,0 :" + beforeLetter);
-            // Console.WriteLine("(letter)0,1 :" + letter);
-            // Console.WriteLine("(after)1,length :" + afterLetter);
-            // Console.WriteLine($"word to send : {wordWithoutLetter}");
-            // Console.WriteLine("");
-            // Console.WriteLine("");
-            //
-            // beforeLetter = wordToPermut.Substring(0, 0 + 1);
-            // letter = wordToPermut.Substring(0 + 1, 1);
-            // afterLetter = wordToPermut.Substring(1 + 1, wordToPermut.Length - 1 - 1);
-            // wordWithoutLetter = beforeLetter + afterLetter;
-            // Console.WriteLine("Select "+ letter);
-            // Console.WriteLine("(before)0,0 :" + beforeLetter);
-            // Console.WriteLine("(letter)0,1 :" + letter);
-            // Console.WriteLine("(after)1,length :" + afterLetter);
-            // Console.WriteLine($"word to send : {wordWithoutLetter}");
-            // Console.WriteLine("");
-            // Console.WriteLine("");
-            //
-            // beforeLetter = wordToPermut.Substring(0, 0 + 2);
-            // letter = wordToPermut.Substring(0 + 2, 1);
-            // afterLetter = wordToPermut.Substring(1 + 2, wordToPermut.Length - 1 - 2);
-            // wordWithoutLetter = beforeLetter + afterLetter;
-            // Console.WriteLine("Select "+ letter);
-            // Console.WriteLine("(before)0,0 :" + beforeLetter);
-            // Console.WriteLine("(letter)0,1 :" + letter);
-            // Console.WriteLine("(after)1,length :" + afterLetter);
-            // Console.WriteLine($"word to send : {wordWithoutLetter}");
-            // Console.WriteLine("");
-            // Console.WriteLine("");
-            
-            //Console.WriteLine(Permutation("abc", 1));
-            Permutation("abcde", 1);
+            Console.WriteLine($"The permutation of {wordToPermut} at number {counter} is :");
+            Permutation(wordToPermut, ref counter);
         }
         
-        public static void Permutation(string _word, int _counter, string _beforeWord = "")
-        //public static string Permutation(string _word, int _counter, string _beforeWord = "")
+        public static void Permutation(string _word, ref int _counter, string _beforeWord = "")
         {
-            string tempWord;
-            
             string beforeLetter;
             string afterLetter;
-            string letter;
+            string wordSelected;
             string wordWithoutLetter;
-            
 
-            for (int indexLastChar =  0; indexLastChar < _word.Length; indexLastChar++)
+            for (int indexChar =  0; indexChar < _word.Length; indexChar++)
             {
-                beforeLetter = _word.Substring(0, 0 + indexLastChar);
-                letter = _beforeWord + _word.Substring(0 + indexLastChar, 1);
-                afterLetter = _word.Substring(1 + indexLastChar, _word.Length - 1 - indexLastChar);
+                beforeLetter = _word.Substring(0, 0 + indexChar);
+                wordSelected = _beforeWord + _word.Substring(0 + indexChar, 1);
+                afterLetter = _word.Substring(1 + indexChar, _word.Length - 1 - indexChar);
                 wordWithoutLetter = beforeLetter + afterLetter;
-                
-                if (letter.ToString().Length == (_word.Length + _beforeWord.Length))
+            
+                if (wordSelected.Length == (_word.Length + _beforeWord.Length))
                 {
-                    Console.WriteLine(letter.ToString());
                     if (_counter == 0)
                     {
-                        Console.WriteLine(letter.ToString());
-                        //return letter.ToString();
+                        Console.WriteLine($"{wordSelected}");
                     }
                     _counter--;
                 }
-                //return Permutation(wordWithoutLetter, _counter, letter);
-                Permutation(wordWithoutLetter, _counter, letter);
+                Permutation(wordWithoutLetter, ref _counter, wordSelected);
             }
         }
     }
