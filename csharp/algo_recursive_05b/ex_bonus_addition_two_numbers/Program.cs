494423c8b0a68757b8a59f010cb69a59964f955e
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-16 16:06:56 +0100

    Add an other algo for recursive addition of two numbers

---
+++ csharp/algo_recursive_05b/ex_bonus_addition_two_numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace ex_bonus_addition_two_numbers
+{
+    internal static class Program
+    {
+        public static void Main(string[] _argv)
+        {
+            int a = 12;
+            int b = 8;
+            
+            Console.WriteLine($"The addition of {a} and {b} is {AdditionTwoNumbers(a, b)}");
+        }
+
+        public static int AdditionTwoNumbers(int _firstNumber, int _secondNumber)
+        {
+            if (_secondNumber == 0)
+            {
+                return _firstNumber;
+            }
+            
+            return 1 + AdditionTwoNumbers(_firstNumber, _secondNumber - 1);
+        }
+    }
+}
