367a1a2ab44d2eae2a4a63b9c215c8d36424d1f1
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-22 08:46:06 +0100

    ex 02 month name

---
+++ csharp/alog_jalon_01/ex_02_month_birthday/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace ex_02_month_birthday
+{
+    class Program
+    {
+        static void Main(string[] _args)
+        {
+            string[] monthsName = new string[]
+            {
+                "Janvier",
+                "Février",
+                "Mars",
+                "Avril",
+                "Mai",
+                "Juin",
+                "Juillet",
+                "Aout",
+                "Septembre",
+                "Octobre",
+                "Novembre",
+                "Decembre"
+            };
+            int userMonthBirthday = 0;
+            bool askAgainUser;
+            
+            do
+            {
+                askAgainUser = false;
+                
+                try
+                {
+                    Console.WriteLine("Please enter the number of you month birthday  :");
+                    userMonthBirthday = int.Parse(Console.ReadLine());
+
+                    if (userMonthBirthday < 1 | userMonthBirthday > 12)
+                    {
+                        throw new ApplicationException(
+                            $"{userMonthBirthday} is not a correct month." +
+                            $"\nPlease enter a correct month number (between 1 and 12)");
+                    }
+                }
+                catch (FormatException error)
+                {
+                    Console.WriteLine($"Error : please enter a correct number ({error.Message})");
+                    askAgainUser = true;
+                }
+                catch (ApplicationException error)
+                {
+                    Console.WriteLine($"Error : {error.Message}");
+                    askAgainUser = true;
+                }
+            } while (askAgainUser);
+
+            for (int numberMonth = 1; numberMonth <= monthsName.Length; numberMonth++)
+            {
+                if (numberMonth == userMonthBirthday)
+                {
+                    Console.WriteLine($"\"{monthsName[numberMonth - 1]}\" (in french sorry) is the {numberMonth} month of the year !");
+                }
+            }
+        }
+    }
+}
