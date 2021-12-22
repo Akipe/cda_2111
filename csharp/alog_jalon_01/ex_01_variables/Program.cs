426606bc1b9b0a8a7242fefa1e859d283aa54614
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2021-12-22 08:25:31 +0100

    Add exercice 01 variables

---
+++ csharp/alog_jalon_01/ex_01_variables/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace ex_01_variables
+{
+    class Program
+    {
+        static void Main(string[] _args)
+        {
+            const string MY_LAST_NAME = "MILLETRE";
+            const string MY_FIRST_NAME = "Julien";
+            const int MY_AGE = 29;
+            const int YEAR_TO_CHECK = 2000;
+
+            int? userAge;
+            
+            Console.WriteLine($"Hello World! I am Mister {MY_FIRST_NAME} {MY_LAST_NAME}, and I am {MY_AGE}.");
+
+            
+            try
+            {
+                Console.WriteLine("What is your age ?");
+
+                userAge = int.Parse(Console.ReadLine());
+
+                if (userAge < 1)
+                {
+                    throw new Exception("Please enter a correct age (>0)");
+                }
+
+                if (userAge > 120)
+                {
+                    Console.WriteLine("You are a super hero!");
+                }
+
+                if (DateTime.Today.Year - userAge >= YEAR_TO_CHECK)
+                {
+                    Console.WriteLine($"You are born after {YEAR_TO_CHECK}.");
+                }
+                else
+                {
+                    Console.WriteLine($"You are born before {YEAR_TO_CHECK}.");
+                }
+            }
+            catch (Exception error)
+            {
+                Console.WriteLine($"Error : Please enter a correct number : {error.Message}");
+            }
+        }
+    }
+}
