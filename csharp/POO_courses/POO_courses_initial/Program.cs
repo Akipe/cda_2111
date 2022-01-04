7321836a08d9789f0265985c3dae2658b7a6e4b3
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-01-04 12:23:27 +0100

    Add demo from initial POO course

---
+++ csharp/POO_courses/POO_courses_initial/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Vehicules;
+
+// Instancier un objet de tpe voiture 
+Voiture maVoiture = new Voiture("Toto");
+
+Console.WriteLine(maVoiture);
+maVoiture.Couleurv2 = "jaune";
+maVoiture.Poids = 250;
+maVoiture.Puissance = 100;
+Console.WriteLine(maVoiture);
+maVoiture.MoteurAllume = true;
+maVoiture.Deplacer(50);
+Console.WriteLine(maVoiture);
