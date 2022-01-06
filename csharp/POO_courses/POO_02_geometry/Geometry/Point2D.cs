54bc5eefe456f64351e1eb47ea34a3fef40d5817
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-01-06 15:05:17 +0100

    Add geometry course

---
+++ csharp/POO_courses/POO_02_geometry/Geometry/Point2D.cs
@@ -0,0 +1,54 @@
+﻿using System.Text;
+
+namespace POO_02_geometry.Geometry;
+
+public class Point2D
+{
+    private int _x;
+    private int _y;
+
+    public Point2D(int x = 0, int y = 0)
+    {
+        SetCoordinates(x, y);
+    }
+
+    /// <summary>
+    /// Coordinates of the abscissa
+    /// </summary>
+    public int X
+    {
+        get => _x;
+        set => _x = value;
+    }
+
+    /// <summary>
+    /// Coordinates of ordinate
+    /// </summary>
+    public int Y
+    {
+        get => _y;
+        set => _y = value;
+    }
+
+    public virtual void SetCoordinates(int x, int y)
+    {
+        X = x;
+        Y = y;
+    }
+
+    public void AddToCoordinates(int howManyToX, int howManyToY)
+    {
+        SetCoordinates(X += howManyToX, Y += howManyToY);
+    }
+    
+    public override string ToString()
+    {
+        StringBuilder pointStatusBuild = new StringBuilder();
+
+        pointStatusBuild.Append("Point coordinates :\n");
+        pointStatusBuild.Append($"- abscissa (x): {X}\n");
+        pointStatusBuild.Append($"- ordinate (y): {Y}");
+        
+        return pointStatusBuild.ToString();
+    }
+}
---
+++ csharp/POO_courses/POO_02_geometry/Geometry/Segment2D.cs
@@ -0,0 +1,89 @@
+﻿using System.Text;
+
+namespace POO_02_geometry.Geometry;
+
+public class Segment2D
+{
+    private Point2D _firstPoint;
+    private Point2D _secondPoint;
+
+    public Segment2D(Point2D firstPoint, Point2D secondPoint)
+    {
+        FirstPoint = firstPoint;
+        SecondPoint = secondPoint;
+    }
+
+    public Segment2D(int firstPointX, int firstPointY, int secondPointX, int secondPointY)
+    {
+        FirstPoint = new Point2D(firstPointX, firstPointY) ;
+        SecondPoint = new Point2D(secondPointX, secondPointY);
+    }
+
+    public Point2D FirstPoint
+    {
+        get => _firstPoint;
+        set => _firstPoint = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public Point2D SecondPoint
+    {
+        get => _secondPoint;
+        set => _secondPoint = value ?? throw new ArgumentNullException(nameof(value));
+    }
+
+    public void ChangeCoordinatesPoints(
+        int coordinateFirstPointX,
+        int coordinateFirstPointY,
+        int coordinateSecondPointX,
+        int coordinateSecondPointY)
+    {
+        FirstPoint.SetCoordinates(coordinateFirstPointX, coordinateFirstPointY);
+        FirstPoint.SetCoordinates(coordinateSecondPointX, coordinateSecondPointY);
+    }
+
+    public void AddToPointsCoordinates(int howManyToX = 0, int howManyToY = 0)
+    {
+        FirstPoint.AddToCoordinates(howManyToX, howManyToY);
+        SecondPoint.AddToCoordinates(howManyToX, howManyToY);
+    }
+
+    public void MovePointToCoordinate(int x, int y, bool moveFirstPoint = true)
+    {
+        int distanceAddToX;
+        int distanceAddToY;
+        Point2D originPoint;
+        Point2D otherPoint;
+        
+        if (moveFirstPoint)
+        {
+            originPoint = FirstPoint;
+            otherPoint = SecondPoint;
+        }
+        else
+        {
+            originPoint = SecondPoint;
+            otherPoint = FirstPoint;
+        }
+        
+        originPoint.SetCoordinates(x, y);
+        distanceAddToX = GetDistanceFromTwoCoordinates(x, FirstPoint.X);
+        distanceAddToY = GetDistanceFromTwoCoordinates(y, FirstPoint.Y);
+        otherPoint.AddToCoordinates(distanceAddToX, distanceAddToY);
+    }
+
+    private int GetDistanceFromTwoCoordinates(int origin, int destination)
+    {
+        return destination - origin;
+    }
+
+    public override string ToString()
+    {
+        StringBuilder messageInCreating = new StringBuilder();
+
+        messageInCreating.Append("Information about the segment :\n");
+        messageInCreating.Append($"Coordinates of first point : {FirstPoint}\n");
+        messageInCreating.Append($"Coordinates of second point : {SecondPoint}");
+
+        return messageInCreating.ToString();
+    }
+}
