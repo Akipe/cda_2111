397244d67f5b72bc9a687c91c0da22a826770e6a
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-01-17 15:59:15 +0100

    Add initial RussianDolls class

---
+++ csharp/POO_exercices/ex_03_russian_dolls/RussianDolls.cs
@@ -0,0 +1,52 @@
+﻿namespace ex_03_russian_dolls;
+
+public class RussianDolls
+{
+    private bool _isOpen;
+    private short _size;
+    private RussianDolls? _inRussianDolls;
+    private RussianDolls? _containDolls;
+
+    public RussianDolls()
+    {
+    }
+
+    public bool IsOpen
+    {
+        get => _isOpen;
+        private set => _isOpen = value;
+    }
+
+    public short Size
+    {
+        get => _size;
+        init => _size = value;
+    }
+
+    public void Open()
+    {
+    }
+
+    public void Close()
+    {
+    }
+
+    public void PutInt(RussianDolls russianDolls)
+    {
+    }
+
+    public void GetOutOf(RussianDolls russianDolls)
+    {
+        
+    }
+
+    private bool IsInRussianDools()
+    {
+        
+    }
+
+    private bool HasRussianDools()
+    {
+        
+    }
+}
