586d778e078a4215f87fedfabe897cc91d1e61fa
Julien Milletre Akipe <code.julien@milletre.fr>
2022-01-18 16:11:22 +0100

    not working for ex 03 Russian dolls with null test

--- csharp/POO_exercices/ex_03_russian_dolls/Program.cs
+++ csharp/POO_exercices/ex_03_russian_dolls/Program.cs
@@ -1,1 +1,88 @@
-﻿
+﻿using System.Diagnostics;
+
+namespace ex_03_russian_dolls;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        RussianDolls p1 = new RussianDolls(1);
+        RussianDolls p2 = new RussianDolls(2);
+        RussianDolls p3 = new RussianDolls(3);
+
+        try
+        {
+            p1.Open();
+
+            try
+            {
+                p2.Close();
+            }
+            catch (ApplicationException error)
+            {
+                Console.Error.WriteLine($"Error test : {error.Message}");
+            }
+
+            try
+            {
+                p2.Open();
+                p2.Open();
+            }
+            catch (ApplicationException error)
+            {
+                Console.Error.WriteLine($"Error test  : {error.Message}");
+            }
+            
+            try
+            {
+                p1.PutIn(p2);
+            }
+            catch (ApplicationException error)
+            {
+                Console.Error.WriteLine($"Error test  : {error.Message}");
+            }
+            
+            p2.Close();
+            p1.Close();
+            
+            p2.Open();
+            p1.PutIn(p2);
+            Console.WriteLine("Test");
+            p2.Close();
+            p3.Open();
+            p2.PutIn(p3);
+
+            p2.GetOutOf(p3);
+            p1.GetOutOf(p2);
+
+            try
+            {
+                p2.PutIn(p1);
+            }
+            catch (ApplicationException error)
+            {
+                Console.Error.WriteLine($"Error test  : {error.Message}");
+            }
+
+            try
+            {
+                p2.PutIn(p1);
+            }
+            catch (ApplicationException error)
+            {
+                Console.Error.WriteLine($"Error test  : {error.Message}");
+            }
+        }
+        catch (ApplicationException error)
+        {
+            Console.Error.WriteLine($"ERROR : {error.Message}");
+        }
+
+    }
+
+    public static void ShowStatusOpenRussianDoll(RussianDolls russianDoll)
+    {
+        Console.WriteLine(
+            $"This Russian doll is {(russianDoll.IsOpen ? "open" : "close")}.");
+    }
+}
--- csharp/POO_exercices/ex_03_russian_dolls/RussianDolls.cs
+++ csharp/POO_exercices/ex_03_russian_dolls/RussianDolls.cs
@@ -2,33 +2,33 @@
 
 public class RussianDolls
 {
-    private bool _isOpen;
-    private short _size;
     private RussianDolls? _inRussianDolls;
     private RussianDolls? _containDolls;
 
-    public RussianDolls(short size)
+    public RussianDolls(byte size)
     {
         Size = size;
+        IsOpen = false;
+        _inRussianDolls = null;
+        _containDolls = null;
     }
 
-    public bool IsOpen
-    {
-        get => _isOpen;
-        private set => _isOpen = value;
-    }
+    public bool IsOpen { get; private set; }
 
-    public short Size
-    {
-        get => _size;
-        init => _size = value;
-    }
+    public byte Size { get; init; }
 
     public void Open()
     {
         if (IsOpen)
         {
-            throw new ApplicationException("You can't open an already open Russian dolls");
+            throw new ApplicationException(
+                "This Russian doll is already open.");
+        }
+
+        if (IsInRussianDools())
+        {
+            throw new ApplicationException(
+                "You can't open this Russian doll because it is inside an other one.");
         }
 
         IsOpen = true;
@@ -38,55 +38,81 @@ public class RussianDolls
     {
         if (!IsOpen)
         {
-            throw new ApplicationException("You can't close an already close Russian dolls");
+            throw new ApplicationException(
+                "This Russian doll is already close.");
+        }
+        
+        if (IsInRussianDools())
+        {
+            throw new ApplicationException(
+                "You can't close this Russian doll because it is inside an other one.");
         }
 
         IsOpen = false;
     }
 
-    public void PutInt(RussianDolls russianDolls)
+    public void PutIn(RussianDolls russianDolls)
     {
         if (IsOpen)
         {
-            throw new ApplicationException("");
+            throw new ApplicationException(
+                "You have to close this Russian doll before to put in an other.");
         }
 
         if (IsInRussianDools())
         {
-            throw new ApplicationException();
+            throw new ApplicationException(
+                "This Russian doll is already in an other Russian doll.");
         }
 
         if (!russianDolls.IsOpen)
         {
-            throw new ApplicationException();
+            throw new ApplicationException(
+                "The other Russian doll has to be open to put this Russian doll inside it.");
         }
 
-        if (!russianDolls.HasRussianDools())
+        if (!(russianDolls.HasRussianDools()))
         {
-            throw new ApplicationException();
+            throw new ApplicationException(
+                "The other Russian doll has already an other Russian doll inside it.");
         }
         
         if (IsSmallerThanRussianDools(russianDolls))
         {
-            throw new ApplicationException("You can't put this russian dolls inside an other smaller russian dolls.");
+            throw new ApplicationException(
+                "The other Russian doll is smaller than this one.");
         }
 
         _inRussianDolls = russianDolls;
+        russianDolls._containDolls = this;
     }
 
     public void GetOutOf(RussianDolls russianDolls)
     {
-        
+        if (!(_inRussianDolls == russianDolls))
+        {
+            throw new ApplicationException(
+                "This Russian doll is not inside the other Russian doll.");
+        }
+
+        if (!_inRussianDolls.IsOpen)
+        {
+            throw new ApplicationException(
+                "You have to open the other Russian doll to get out this.");
+        }
+
+        _inRussianDolls = null;
+        russianDolls._containDolls = null;
     }
 
     public bool IsInRussianDools()
     {
-        
+        return _inRussianDolls is not null;
     }
 
     public bool HasRussianDools()
     {
-        
+        return _inRussianDolls is not null;
     }
 
     private bool IsSmallerThanRussianDools(RussianDolls russianDolls)
