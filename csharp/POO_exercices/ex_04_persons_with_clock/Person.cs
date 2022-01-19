e2303ea6cfeeada0096f169e94e11c8ff99e618f
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-01-19 13:27:27 +0100

    Add initials Person & Watch c# class

---
+++ csharp/POO_exercices/ex_04_persons_with_clock/Person.cs
@@ -0,0 +1,41 @@
+﻿namespace ex_04_persons_with_clock;
+
+public class Person
+{
+    private string _name;
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public string Name
+    {
+        get => _name;
+        init => _name = value;
+    }
+
+    public bool HasWatch()
+    {
+    }
+
+    public void WearWatch(Watch watch)
+    {
+    }
+
+    public void RemoveWatch()
+    {
+    }
+
+    public void AskTime(Person person)
+    {
+    }
+
+    public string GiveTime()
+    {
+    }
+
+    public void GiveWatchTo(Person person)
+    {
+    }
+}
---
+++ csharp/POO_exercices/ex_04_persons_with_clock/Watch.cs
@@ -0,0 +1,57 @@
+﻿namespace ex_04_persons_with_clock;
+
+public class Watch
+{
+    public const ushort HOW_MANY_HOURS_IN_DAY = 24;
+    public const ushort HOW_MANY_MINUTES_IN_HOURS = 60;
+    private ushort _currentMinutesOfDay;
+    private Person? _wearBy;
+
+    public Watch(ushort initialHours, ushort initialMinutes)
+    {
+    }
+
+    public Watch(Watch anExistingWatch)
+    {
+    }
+
+    public void IncrementOneMinute()
+    {
+    }
+
+    public ushort GetHours()
+    {
+    }
+
+    public ushort GetMinutes()
+    {
+    }
+
+    public Person? GetWearBy()
+    {
+    }
+
+    public string GetTime()
+    {
+    }
+
+    private void SetWearBy(Person person)
+    {
+    }
+
+    private void RemoveWearFromPerson()
+    {
+        
+    }
+
+    private ushort CurrentMinutesOfDay
+    {
+        get => _currentMinutesOfDay;
+        set => _currentMinutesOfDay = value;
+    }
+
+    private void AutomaticallyIncrementMinutes()
+    {
+        
+    }
+}
