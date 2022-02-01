8cb71916f6f13e7e62ad322cc9b99f5f02113240
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-02-01 16:25:08 +0100

    Add part of correction ex ONF

---
+++ csharp/poo_group_onf/onf_correction/Parcel.cs
@@ -0,0 +1,14 @@
+﻿namespace onf_correction;
+
+public class Parcel
+{
+    private readonly int _id;
+    private readonly List<Arbre> _arbres;
+    private int _surface;
+
+    public Parcel(int id)
+    {
+        this._id = _id;
+        this._arbres = new List<Arbre>();
+    }
+}
---
+++ csharp/poo_group_onf/onf_correction/TypeArbre.cs
@@ -0,0 +1,7 @@
+﻿namespace onf_correction;
+
+public enum TypeArbre : int
+{
+    RESINEUX = 0,
+    FEUILLU = 1
+}
