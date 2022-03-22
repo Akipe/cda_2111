3251a257ad7ab8c973d00294f49c422123f37696
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-03-22 15:57:45 +0100

    Add initial API from course

---
+++ web/backend/ApiWeb/ApiCereals/Models/Cereal.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiCereals.Models
+{
+    [Table("tata")]
+    public class Cereal
+    {
+        [Key]
+        public int Id { get; set; } // Norme MS
+
+        [Required(ErrorMessage = "Le champs est requis")]
+        [StringLength(32, MinimumLength = 2)]
+        [RegularExpression(@"^\p{L}{2,32}$")]
+        public string Name { get; set; }
+    }
+}
