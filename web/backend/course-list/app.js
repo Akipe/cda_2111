96e690476063b05052e9f6813b96c65b642861ba
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-03-29 17:00:25 +0200

    Add some courses for web frontend

---
+++ web/backend/course-list/app.js
@@ -0,0 +1,7 @@
+const link = document.querySelector('a');
+
+link.textContent = "yolo";
+link.href = "yolo";
+
+let text = document.createTextNode("test de test");
+document.querySelector("section").appendChild(text);
--- web/backend/web-chat/assets/main.js
+++ web/backend/web-chat/assets/main.js
@@ -1,7 +1,29 @@
-const element = document.getElementById('logo');
+function generateUserTable(json)
+{
+    for (let user of json) {
+        let tr = document.createElement('tr');
+        let td = document.createElement('td');
+        td.textContent = user.user_name;
 
-console.log(element);
+        tr.appendChild(td);
 
-element.style.color = 'red';
-element.style.fontSize = '200px';
+        document.getElementById('userTable').appendChild(tr);
+    }
+}
 
+// Pour le fetch, on peut définir un accés local ou distant
+fetch("https://api.devoldere.net/dataset/users.json")
+// Le role du prochain then sera de formater la requete, en JSON, binaire, etc
+.then(response => {
+    console.log(response);
+    return response.json();
+})
+// On récupére le JSON dans le paramètre "json"
+.then(json => {
+    console.log(json);
+    generateUserTable(json);
+})
+// On affiche l'erreur en cas d'échec
+.catch(error => {
+    console.error(error);
+})
