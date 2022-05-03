40890ce383d8432e71728bc1bed9474f8c430e36
Julien Milletre <5948003+Akipe@users.noreply.github.com>
2022-05-03 11:31:32 +0200

    todo

--- web/backend/cours-web/express_demo_ts_paul/routes/staticFiles.ts
+++ web/backend/cours-web/express_demo_ts_paul/routes/staticFiles.ts
@@ -2,7 +2,6 @@ import path from "path"
 import express, { Router, Request, Response } from "express"
 import process from "process"
 
-
 const staticFilesRootPath = path.join(process.cwd(), 'public')
 
 export const staticFiles = (router: Router) => {
