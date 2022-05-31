a2bc5f3ca7c85a1c4dce0e7922b5fd81fa2ea687
Julien Milletre Akipe <code.julien@milletre.fr>
2022-05-31 16:01:12 +0200

    Add simple addition calculate

---
+++ desktop/CourseWinForm/02_simple_addition/SimpleAddition.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _02_simple_addition
+{
+    internal class SimpleAddition
+    {
+        private int ResultAddition { get; set; }
+        public string PrintableResult { get; private set; }
+
+        public SimpleAddition()
+        {
+            this.Reset();
+        }
+
+        public void Add(int number)
+        {
+            this.ResultAddition += number;
+            this.PrintableResult += number + "+";
+        }
+
+        public void Calculate()
+        {
+            this.PrintableResult += " = " + ResultAddition + "+";
+        }
+
+        public void Reset()
+        {
+            this.ResultAddition = 0;
+            this.PrintableResult = "";
+        }
+    }
+}
