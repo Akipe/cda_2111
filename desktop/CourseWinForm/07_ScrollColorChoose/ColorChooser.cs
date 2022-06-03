8f9e4b386b7d39a16c9bdb11b3173075adb97c93
Julien Milletre Akipe <code.julien@milletre.fr>
2022-06-03 11:56:38 +0200

    Add Scroll Color chooser

---
+++ desktop/CourseWinForm/07_ScrollColorChoose/ColorChooser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _07_ScrollColorChoose
+{
+    public class ColorChooser
+    {
+        public const byte MIN_COLOR_LEVEL = 0;
+        public const byte MAX_COLOR_LEVEL = 255;
+
+        public byte Red { get; set; }
+        public byte Green { get; set; }
+        public byte Blue { get; set; }
+
+        public ColorChooser()
+        {
+            Red = 0;
+            Green = 0;
+            Blue = 0;
+        }
+
+        public Color GetFinalColor()
+        {
+            return Color.FromArgb(Red, Green, Blue);
+        }
+    }
+}
---
+++ desktop/CourseWinForm/07_ScrollColorChoose/ScrollColorChooseMainForm.cs
@@ -0,0 +1,10 @@
+namespace _07_ScrollColorChoose
+{
+    public partial class ScrollColorChooseMainForm : Form
+    {
+        public ScrollColorChooseMainForm()
+        {
+            InitializeComponent();
+        }
+    }
+}
