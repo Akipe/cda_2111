df340e8db6f6f9fcd977b19d3547bec82c5b92a3
Julien Milletre Akipe <code.julien@milletre.fr>
2022-11-10 11:21:54 +0100

    Add initial logic

--- mobile/Counter/app/src/main/java/com/akipe/counter/MainActivity.java
+++ mobile/Counter/app/src/main/java/com/akipe/counter/MainActivity.java
@@ -3,12 +3,28 @@ package com.akipe.counter;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Integer number;
+    private EditText numberBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        this.number = 10;
+        this.numberBtn = findViewById(R.id.numberField);
+
+        this.numberBtn.setText(number.toString());
+    }
+
+    public void incrementNumber(View v)
+    {
+        this.number++;
+        this.numberBtn.setText(number);
     }
 }
