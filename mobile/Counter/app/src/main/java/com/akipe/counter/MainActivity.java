dbc8558fbe9a77f5cbe987dabfa071ed03b90b56
Julien Milletre Akipe <code.julien@milletre.fr>
2022-11-13 16:01:08 +0100

    Add final version exercice

--- mobile/Counter/app/src/main/java/com/akipe/counter/MainActivity.java
+++ mobile/Counter/app/src/main/java/com/akipe/counter/MainActivity.java
@@ -1,15 +1,20 @@
 package com.akipe.counter;
 
-import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     private Integer number;
     private EditText numberBtn;
+    private Button incrementBtn;
+    private Button decrementBtn;
+    private Button exitBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,12 +24,38 @@ public class MainActivity extends AppCompatActivity {
         this.number = 10;
         this.numberBtn = findViewById(R.id.numberField);
 
-        this.numberBtn.setText(number.toString());
+        this.numberBtn.setText(this.number.toString());
+
+        decrementBtn = findViewById(R.id.decrement_number_btn);
+        incrementBtn = findViewById(R.id.increment_number_btn);
+        exitBtn = findViewById(R.id.exit_btn);
+
+        decrementBtn.setOnClickListener(
+            this::manageNumber
+        );
+        incrementBtn.setOnClickListener(
+            this::manageNumber
+        );
+        exitBtn.setOnClickListener(
+            this::exit
+        );
     }
 
-    public void incrementNumber(View v)
+    // Le listener doit avoir cette signature
+    public void manageNumber(View view)
     {
-        this.number++;
-        this.numberBtn.setText(number);
+        if (view.getId() == R.id.increment_number_btn) {
+            this.number++;
+        } else {
+            this.number--;
+        }
+
+        this.numberBtn.setText(this.number.toString());
+    }
+
+    public void exit(View view)
+    {
+        this.finishAndRemoveTask();
+        System.exit(0);
     }
 }
